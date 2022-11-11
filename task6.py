8d66c9e497756a57994337180ad10ec6716784c4
TsvetkovaAY <115409533+TsvetkovaAY@users.noreply.github.com>
2022-11-11 13:38:46 +0300

    Update task6.py

--- task6.py
+++ task6.py
@@ -2,6 +2,6 @@ list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 3
 max_index = 0
 for i in range(len(list_numbers)):
     if list_numbers[i] > list_numbers[max_index]:
-        max_index = i;
+        max_index = i
 list_numbers[max_index], list_numbers[-1] = list_numbers[-1], list_numbers[max_index]
 print(list_numbers)
