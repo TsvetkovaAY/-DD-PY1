f615d2364010bef3a4764360047aad45983a2efe
TsvetkovaAY <115409533+TsvetkovaAY@users.noreply.github.com>
2022-11-13 19:15:19 +0300

    Update task8.py

--- task8.py
+++ task8.py
@@ -4,8 +4,9 @@ spend = 6000
 increase = 0.05
 
 month = 0  # количество месяцев, которое можно прожить
+
 while money_capital >= spend:
-    spend_new = (increase * (month + 1) + 1) * spend
-    money_capital = money_capital - spend_new + salary
+    spend = (increase + 1) ** month * spend  #изменяем траты каждый месяц согласно условию
+    money_capital = money_capital - spend + salary
     month += 1
 print(month)
