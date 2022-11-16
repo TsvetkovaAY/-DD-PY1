7a916be2edc3bb7354a5bacb0f2ba64b17052dc0
TsvetkovaAY <115409533+TsvetkovaAY@users.noreply.github.com>
2022-11-16 23:04:43 +0300

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,8 +1,7 @@
 import random
 import string
 
-def get_random_password() -> str:
-    n = 8
+def get_random_password(n = 8) -> str:
     alphabet = string.ascii_letters + string.digits
     password = ''.join(random.sample(alphabet, n))
     return password
