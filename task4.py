edebb5b2003b711d3f04dcc947cecc87f9210002
TsvetkovaAY <115409533+TsvetkovaAY@users.noreply.github.com>
2022-11-27 20:22:47 +0300

    Лабораторная работа 6

---
+++ task4.py
@@ -0,0 +1,22 @@
+import json
+
+INPUT_FILE = "input.csv"
+
+
+def csv_to_list_dict(INPUT_FILE) -> list[dict]:
+    rows = []
+    list_dict = []
+    delimiter = ','
+    with open(INPUT_FILE, 'r') as f:
+        res = f.readlines()
+        headers = res[0].rstrip().split(delimiter)
+        for r in res[1::]:
+            rows.append(r.rstrip().split(delimiter))
+
+        for r in rows:
+            new_dict = {k: i for k, i in zip(headers, r)}
+            list_dict.append(new_dict)
+    return list_dict
+
+
+print(json.dumps(csv_to_list_dict(INPUT_FILE), indent=4))
